b130538b19dff2fb533bf25af026f93d27f6ae6d
yucicheung@gmail.com <yucicheung@gmail.com>
2018-07-10 10:29:25 +0800

    Add hello pro as qt example.

---
+++ xmake/qmake/hello/hello.h
@@ -0,0 +1,16 @@
+#ifndef HELLO
+#define HELLO
+
+#endif // HELLO
+
+#include<iostream>
+#include<string>
+
+class hello
+{
+public:
+    hello();
+    ~hello();
+private:
+    std::string greetingMsg;
+};
